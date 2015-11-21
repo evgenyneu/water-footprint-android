b46bf036e9ed9df75151281acf31f2e97bbc58c4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-11-21 13:37:48 +1100

    Learn: showing simple list

--- app/src/main/java/com/evgenii/waterfootprint/ProductListActivity.java
+++ app/src/main/java/com/evgenii/waterfootprint/ProductListActivity.java
@@ -25,13 +25,7 @@ public class ProductListActivity extends AppCompatActivity {
                 "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
                 "Android", "iPhone", "WindowsMobile" };
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
+        final MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
         listview.setAdapter(adapter);
     }
 
