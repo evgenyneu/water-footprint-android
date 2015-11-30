dd95ec68531708be1cd0a1188c5300a4dee5ac80
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-11-30 14:48:06 +1100

    Search products

--- app/src/androidTest/java/com/evgenii/waterfootprint/core/DataSearchTests.java
+++ app/src/androidTest/java/com/evgenii/waterfootprint/core/DataSearchTests.java
@@ -2,7 +2,48 @@ package com.evgenii.waterfootprint.core;
 
 import android.test.AndroidTestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataSearchTests extends AndroidTestCase {
+
+    // Match sentence
+    // ---------------------
+
+    public void testDoesMatchSentence() {
+        ProductModel model = new ProductModel();
+        model.name = "Beef";
+        model.synonyms = "Cow meat";
+
+        List<String> words = new ArrayList<String>();
+        words.add("beef");
+        words.add("meat");
+
+        assertTrue(DataSearch.doesMatchSentence(model, words));
+    }
+
+    public void testDoesMatchSentence_oneWord() {
+        ProductModel model = new ProductModel();
+        model.name = "Beef";
+        model.synonyms = "Cow meat";
+
+        List<String> words = new ArrayList<String>();
+        words.add("bee");
+
+        assertTrue(DataSearch.doesMatchSentence(model, words));
+    }
+
+    public void testDoesMatchSentence_noMatch() {
+        ProductModel model = new ProductModel();
+        model.name = "Beef";
+        model.synonyms = "Cow meat";
+
+        List<String> words = new ArrayList<String>();
+        words.add("checken");
+        words.add("meat");
+
+        assertFalse(DataSearch.doesMatchSentence(model, words));
+    }
 
     // Match single word
     // ---------------------
@@ -24,5 +65,15 @@ public class DataSearchTests extends AndroidTestCase {
         model.synonyms = "Чили, красный, чёрный";
 
         assertTrue(DataSearch.doesMatchSingleWord(model, "черный"));
+    }
+
+    public void testDoesMatchSingleWord_emptySynonym() {
+        ProductModel model = new ProductModel();
+        model.name = "Перец свежий";
+        model.synonyms = "";
+
+        assertTrue(DataSearch.doesMatchSingleWord(model, ""));
+        assertTrue(DataSearch.doesMatchSingleWord(model, "Перец"));
+        assertFalse(DataSearch.doesMatchSingleWord(model, "Nothing"));
     }
 }
--- app/src/main/java/com/evgenii/waterfootprint/core/DataSearch.java
+++ app/src/main/java/com/evgenii/waterfootprint/core/DataSearch.java
@@ -2,9 +2,22 @@ package com.evgenii.waterfootprint.core;
 
 import com.evgenii.waterfootprint.utils.WaterString;
 
+import java.util.List;
+
 public class DataSearch {
+
+    public static Boolean doesMatchSentence(ProductModel model, List<String> words) {
+        for (String word : words) {
+            if (!doesMatchSingleWord(model, word)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static Boolean doesMatchSingleWord(ProductModel model, String word) {
-        
+
         if (WaterString.containsIgnoreCaseAndDiacritic(model.name, word)) {
             return true;
         }
